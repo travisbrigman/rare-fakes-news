17201ffd0e96a342cfc4ba38d14eca8e30d8fd62
justha <nganha42@gmail.com>
2020-10-22 15:57:15 -0500

    routes to MyProfile

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { TagProvider } from "./Tags/TagProvider"
+import { TagForm } from "./Tags/TagForm"
+import { TagList } from "./Tags/TagList"
 import {HomeList} from "./Profiles/HomeList"
 import { UserProvider } from "./Profiles/UserProvider"
 import { UserDetail } from "./Profiles/UserDetail"
@@ -11,6 +14,16 @@ export const ApplicationViews = () => {
             lineHeight: "1.75rem"
         }}>
         </main>
+
+        <TagProvider>
+            <Route exact path="/tags/create" render={(props) => {
+                return <TagForm {...props}/>
+            }} />
+            <Route exact path="/tags" render={(props) => {
+                return <TagList {...props} />
+            }} />
+            
+        </TagProvider>
         <Route exact path="/home">
             <HomeList />
         </Route>
---
+++ src/components/Tags/TagForm.js
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect } from "react"
+import { TagContext } from "./TagProvider"
+
+
+
+export const TagForm = (props) => {
+    // Use the required context providers for data
+    const { tags, getTags, createTag, tag, setTag } = useContext(TagContext)
+
+
+
+    const handleControlledInputChange = (event) => {
+        /*
+            When changing a state object or array, always create a new one
+            and change state instead of modifying current one
+        */
+        const newTag = Object.assign({}, tag)          // Create copy
+        newTag[event.target.name] = event.target.value    // Modify copy
+        setTag(newTag)                                 // Set copy as new state
+    }
+
+    // Get animals from API when component initializes
+    useEffect(() => {
+        getTags()
+    }, [])
+
+
+    const constructNewTag = () => {
+                // POST
+                createTag({
+                    tag: tag.tag
+                })
+                    .then(() => props.history.push("/tags")) //takes user to tag list page
+            }
+    
+    
+
+    return (
+        <form className="tagForm">
+            <h2 className="tagForm__title">Tag form</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="tag">Enter tag name: </label>
+                    <input type="text" name="tag" required autoFocus className="form-control"
+                        placeholder="ex: sports, politics, etc"
+                        onChange={handleControlledInputChange}
+                    />
+                </div>
+            </fieldset>
+            <button type="submit"
+                onClick={evt => {
+                    evt.preventDefault()
+                    constructNewTag()
+                }}
+                className="btn btn-primary">
+                Create Tag
+            </button>
+        </form>
+    )
+}
