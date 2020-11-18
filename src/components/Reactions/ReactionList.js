810a89737367c0c7759a8df4a029c447ab56cad5
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-11-18 09:22:02 -0600

    added a comment to reaction list

--- src/components/Reactions/ReactionList.js
+++ src/components/Reactions/ReactionList.js
@@ -8,22 +8,19 @@ import "./Reaction.css"
 export const ReactionList = (props) => {
     const { getReactions, reactions } = useContext(ReactionContext)
 
-
     // Initialization effect hook -> Go get tag data
     useEffect(() => {
         getReactions()
-
     }, [])
 
     return (
         <div style={{ margin: "1rem" }} className="reactionContainer">
             <h3>Reactions:</h3>
             <div className="reactions">
-                {
+                { //map through the reactions and pass the reaction obj and props down to the child component
                     reactions.map(reaction => <Reaction key={reaction.id} {...props} reaction={reaction} />)
                 }
             </div>
-
         </div>
     )
 }
